601f9fad95ea4eac7c130f6b25e9f6f62ab125a2
Kyle Bissoondia <biss.kyle@gmail.com>
2020-10-27 17:13:29 -0400

    commit tree 910b1a1b56c5c321db3ab7d1b0f6b67fdb353fd6

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+    tv_shows =[ "wow" , "I"]
+    tv_shows.sort
 end
 
 def using_reverse(array)
-
+    tv_shows =["wow" , "arrays!"]
+    tv_shows.reverse
 end
 
 def using_first(array)
-
+    tv_shows =["wow" , "arrays!"]
+    tv_shows.first
 end
 
 def using_last(array)
-
+    tv_shows =["wow" , "arrays!"]
+    tv_shows.last
 end
 
 def using_size(array)
-
+    tv_shows =["wow" , "arrays!" , "class" , "method" , "action" , "work"]
+    tv_shows.size
 end
